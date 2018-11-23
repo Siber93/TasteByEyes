7b055ea262907d35c4ba543f8b1fd2f56782a6aa
Simone Bertolini <siber93@hotmail.it>
2018-11-23 11:52:55 +0100

    Inserita tutta la navigazione in Navigator

---
+++ TasteByEyes/Assets/Script/Navigator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour {
+
+    /// <summary>
+    /// Distrugge tutte le istanze della scena indicata
+    /// </summary>
+    public void UnloadScene(string name)
+    {
+        SceneManager.UnloadSceneAsync(name);
+    }
+
+
+    /// <summary>
+    /// Consente di passare alla visualizzazione 3D caricando il file specificato
+    /// </summary>
+    /// <param name="fileToLoad"> File da caricare nel 3D</param>
+    public void Load3DScene(string fileToLoad)
+    {
+        ObjectLoader.FileToLoad = fileToLoad;
+        SceneManager.LoadScene("3DView");
+    }
+
+    /// <summary>
+    /// Carica una scena generica indicata attraverso il suo nome
+    /// </summary>
+    /// <param name="scene">Nome della scena di istanziare</param>
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
--- TasteByEyes/Assets/Script/VuforiaSettingsManager.cs
+++ TasteByEyes/Assets/Script/VuforiaSettingsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vuforia;
 
 public class VuforiaSettingsManager : MonoBehaviour {
@@ -26,4 +27,5 @@ public class VuforiaSettingsManager : MonoBehaviour {
             DigitalEyewearARController.Instance.SetSeeThroughConfiguration(DigitalEyewearARController.SeeThroughConfiguration.Vuforia);
         }
     }
+    
 }
